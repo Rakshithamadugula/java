98cbbf443eabd6b2902085f37fcf950f7eef6935
rakshu <rakshithamadugula2307@gmail.com>
2024-12-04 13:29:44 +0530

    w

--- src/basic/hi.java
+++ src/basic/hi.java
@@ -4,7 +4,7 @@ public class hi {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("f");
+System.out.println("f");// code change
 	}
 public void m1()
 {
